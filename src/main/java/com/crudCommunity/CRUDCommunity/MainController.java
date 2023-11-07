83b09f759271c678f9003b6f01ed047f2b48cc57
정수현 <windseed71@naver.com>
2023-11-07 23:13:13 +0900

    리팩터링 : 이름변경, 경로변경
    save -> write

--- src/main/java/com/crudCommunity/CRUDCommunity/MainController.java
+++ src/main/java/com/crudCommunity/CRUDCommunity/MainController.java
@@ -3,12 +3,10 @@ package com.crudCommunity.CRUDCommunity;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.Map;
@@ -24,19 +22,24 @@ public class MainController {
     {
         return "index.html";
     }
-	@GetMapping("/save")
-    public String saveForm()
+	@GetMapping("/write")
+    public String writeForm()
     {
-        return "save.html";
+        return "write.html";
     }
-    @PostMapping("/save")
-    public String save(@RequestParam("postWriter") String postWriter,
+    @PostMapping("/write")
+    public String write(@RequestParam("postWriter") String postWriter,
                        @RequestParam("postPassword") String postPassword,
                        @RequestParam("postTitle") String postTitle,
                        @RequestParam("postText") String postText)
     {
-        System.out.println("save");
+        System.out.println("write");
         mainModel.AddPost(postWriter,postPassword,postTitle,postText);
+        return "redirect:/main";
+    }
+    @GetMapping("/list/{postNumber}")
+    public String GetPost(@PathVariable int postNumber)
+    {
         return "redirect:/main";
     }
     public static void main(String[] args) {
